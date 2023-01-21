3d0a151acbf20b579a647ecf5c6d49e0c35e90af
Дмитрий ПоскОтинов <demitry.poskotinov@gmail.com>
2023-01-21 23:16:49 +0300

    add working version without classes

--- empty.py
+++ empty.py
@@ -1,20 +1,23 @@
 from math import sqrt
 
-message = 'Добро пожаловать в самую лучшую программу для'
-'вычисления квадратного корня из заданного числа'
+message = 'Добро пожаловать в самую лучшую программу для '\
+    'вычисления квадратного корня из заданного числа'
 print(message)
 
 
-def Calculate_Square_Root(number):
-    """Вычисляет квадратный корень"""
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
     return sqrt(number)
 
 
 def calc(your_number):
+    """Основная функция."""
     if your_number <= 0:
         return
-    print(f' Мы вычислили квадратный корень из введённого вами числа.'
-          f' Это будет: {Calculate_Square_Root(your_number)}')
+    else:
+        shit = calculate_square_root(your_number)
+        print(f'Мы вычислили квадратный корень из введённого вами числа. '
+              f'Это будет: {shit}')
 
-print(message)
+
 calc(25.5)
--- main.py
+++ main.py
@@ -98,5 +98,5 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class)) 
-    
+    ###проверить гит
 main()
